562b348f94df6b19eb5f662f67e6b8df61399251
Patrick da Costa Cruz <106276838+patrickccruz@users.noreply.github.com>
2023-04-27 12:52:20 -0300

    Update script.js migração info add

--- script.js
+++ script.js
@@ -462,19 +462,13 @@ function textoMigra() {
   document.getElementById("Mresp14").innerHTML = `- APLA: ${Aresp14}`;
 
   let Aresp15 = document.getElementById("ticketFinal").value;
-  document.getElementById(
-    "Mresp15"
-  ).innerHTML = `- Número do Ticket de Finalização dos treinamentos: ${Aresp15}`;
+  document.getElementById("Mresp15").innerHTML = `- Número do Ticket de Finalização dos treinamentos: ${Aresp15}`;
 
   let Aresp16 = document.getElementById("dispensaTicket").value;
-  document.getElementById(
-    "Mresp16"
-  ).innerHTML = `- Houve dispensa de treinamento ?Se sim, qual numero do ticket?: ${Aresp16}`;
+  document.getElementById("Mresp16").innerHTML = `- Houve dispensa de treinamento ?Se sim, qual numero do ticket?: ${Aresp16}`;
 
   let Aresp17 = document.getElementById("Minfo").value;
-  document.getElementById(
-    "Mresp17"
-  ).innerHTML = `- Informações Adicionais ${Aresp17}`;
+  document.getElementById("Mresp17").innerHTML = `- Informações Adicionais ${Aresp17}`;
 }
 
 function copResp3() {
@@ -495,7 +489,7 @@ function copResp3() {
   let resp14 = document.getElementById("Mresp15").textContent;
   let resp15 = document.getElementById("Mresp16").textContent;
   let resp16 = document.getElementById("Mresp17").textContent;
-  const textToCopy = `${resp0}\n${resp1}\n${resp2}\n${resp3}\n${resp4}\n${resp5}\n${resp6}\n${resp7}\n${resp8}\n${resp9}\n${resp10}\n${resp11}\n${resp12}\n${resp13}\n${resp14}\n${resp15}\n${resp16}`;
+  const textToCopy = `${resp0}\n${resp1}\n${resp2}\n${resp3}\n${resp4}\n${resp5}\n${resp6}\n${resp7}\n${resp8}\n${resp9}\n${resp10}\n${resp11}\n${resp12}\n${resp13}\n${resp14}\n${resp15}\n${resp16}\n${resp17}`;
   const textarea = document.createElement("textarea");
   textarea.value = textToCopy;
   document.body.appendChild(textarea);
